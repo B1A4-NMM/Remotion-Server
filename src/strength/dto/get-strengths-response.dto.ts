ff110f3a7c8a12f7c78323d9473f7575b826d768
molcham <130418659+molcham@users.noreply.github.com>
2025-07-08 14:03:16 +0900

    FIX[strength]: 강점 응답 형식 수정완료

--- src/strength/dto/get-strengths-response.dto.ts
+++ src/strength/dto/get-strengths-response.dto.ts
@@ -11,12 +11,26 @@ export class GetStrengthsResponseDto {
   
     @ApiProperty({
       type: 'object',
-      additionalProperties: { type: 'number' },
-      example: { 창의성: 1, 유머: 2 },
+      additionalProperties: 
+      {
+        type: 'object',
+        additionalProperties: { type: 'number' },
+      },
+      example: {
+        지혜: {
+          창의성: 1,
+          호기심: 2,
+        },
+        초월: {
+          유머: 2,
+        },
+      },
+      description:'강점 유형별 상세 강점 카운트',
     })
-    detailCount: Record<string, number>;
+    detailCount: Record<string,Record<string, number>>;
   
-    constructor(typeCount: Record<string, number>, detailCount: Record<string, number>) {
+    constructor(typeCount: Record<string, number>, 
+      detailCount: Record<string,Record<string, number>>,) {
       this.typeCount = typeCount;
       this.detailCount = detailCount;
     }
