b37950858fab10d5d4d3b7d4832ed3f6e7bcbb40
goochul-im <goochul175465@gmail.com>
2025-07-15 20:59:57 +0900

    FIX(routine): 다이어리 루틴 추천 시 루틴 타입도 반환하도록 수정

--- src/routine/dto/recommend-routine.res.ts
+++ src/routine/dto/recommend-routine.res.ts
@@ -1,8 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { RoutineEnum } from '../../enums/routine.enum';
 
 export class RecommendRoutineRes {
   @ApiProperty({ description: '루틴 ID', type: Number, nullable: true })
   routineId?: number | null;
+
+  @ApiProperty({ description: '루틴 타입', enum: RoutineEnum })
+  routineType: RoutineEnum;
 
   @ApiProperty({ description: '루틴 내용', type: String, nullable: true })
   content?: string | null;
